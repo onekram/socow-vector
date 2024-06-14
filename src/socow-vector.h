f8c4499e40991c72178233516386546b9c6908ee
Ivan Kramarenko <ivankramarenko13@gmail.com>
2024-06-15 01:58:21 +0300

    feat: Socow class and some implementations

--- src/socow-vector.h
+++ src/socow-vector.h
@@ -1,6 +1,280 @@
 #pragma once
 
+#include <array>
+#include <cassert>
 #include <cstddef>
+#include <iterator>
+#include <memory>
+
+#include "vector.h"
 
 template <typename T, std::size_t SMALL_SIZE>
-class socow_vector {};
+class socow_vector {
+public:
+  using value_type = T;
+
+  using reference = T&;
+  using const_reference = const T&;
+
+  using pointer = T*;
+  using const_pointer = const T*;
+
+  using iterator = pointer;
+  using const_iterator = const_pointer;
+
+public:
+  socow_vector() : _size(0), _buffer(nullptr) {}
+
+  // O(SMALL_SIZE) / O(1); strong / nothrow
+  socow_vector(const socow_vector& other)
+      : _size(other.size()), _buffer(nullptr) {
+    if (other.is_small_object()) {
+      _data = other._data;
+//      std::copy(std::begin(other._data), std::end(other._data), std::begin(_data))
+    } else {
+      _buffer = other._buffer;
+    }
+  }
+
+  socow_vector(socow_vector&& other) noexcept
+      : _size(other._size)
+      , _buffer(other._buffer) {
+
+    std::swap(_data, other._data);
+    other._buffer = nullptr;
+  }
+
+  // O(SMALL_SIZE) / O(1); strong / nothrow
+  socow_vector& operator=(const socow_vector& other) {
+    if (this != &other) {
+      socow_vector copy(other);
+      swap(copy);
+    }
+    return *this;
+  }
+
+  // ???
+  ~socow_vector() {
+    if (is_small_object()) {
+      for(iterator it = begin(); it != end(); ++it) {
+        it->~value_type();
+      }
+    }
+  }
+
+  // Fields access
+
+  std::size_t size() const {
+    return is_small_object() ? _size : _buffer->size();
+  }
+
+  bool empty() const {
+    return size() == 0;
+  }
+
+  std::size_t capacity() const {
+    return is_small_object() ? SMALL_SIZE : _buffer->capacity();
+  }
+
+  // O(1) / O(size); nothrow / strong
+  pointer data() {
+    if (is_small_object()) {
+      return _data.data();
+    } else {
+      if (_buffer.use_count() > 1) {
+        vector<T> vec = *_buffer;
+        _buffer = std::make_shared<vector<T>>(vec);
+      }
+      return _buffer->data();
+    }
+  }
+
+  // O(1) / O(1); nothrow / nothrow
+  const_pointer data() const noexcept {
+    if (is_small_object()) {
+      return _data.data();
+    } else {
+      return _buffer->data();
+    }
+  }
+
+  void destroy_small() {
+    for(std::size_t i = 0; i < SMALL_SIZE; ++i) {
+      _data[i].~value_type();
+    }
+  }
+
+  // Operations
+
+  // O(1) / O(1)*; nothrow / strong
+  void push_back(const T& value) {
+    if (is_full()) {
+      vector<T> vec(_data, _size);
+//      destroy_small();
+      ++_size;
+      vec.push_back(value);
+      _buffer = std::make_shared<vector<T>>(std::move(vec));
+    } else if (is_small_object()) {
+      _data[_size++] = value;
+    } else {
+      if (_buffer.use_count() > 1) {
+        vector<T> vec = *_buffer;
+        _buffer = std::make_shared<vector<T>>(vec);
+      }
+      _buffer->push_back(value);
+    }
+  }
+
+  // 0(SMALL_SIZE) / 0(size); strong / strong
+  void reserve(std::size_t new_capacity) {
+    if (is_small_object() && _size + new_capacity > SMALL_SIZE) {
+      vector<T> vec(_data, _size);
+      _buffer = std::make_shared<vector<T>>(vec);
+      _buffer->reserve(new_capacity);
+    } else if (!is_small_object()) {
+      if (_buffer.use_count() > 1) {
+        vector<T> vec = *_buffer;
+        _buffer = std::make_shared<vector<T>>(vec);
+      }
+      _buffer->reserve(new_capacity);
+    }
+  }
+
+  // 0(1) / 0(size); nothrow / strong
+  void shrink_to_fit() {
+    if (!is_small_object()) {
+      _buffer->shrink_to_fit();
+    }
+  }
+
+  // O(1) / 0(N); nothrow / nothrow
+  void clear() noexcept {
+    if (!is_small_object()) {
+      if (_buffer.use_count() > 1) {
+        vector<T> vec = *_buffer;
+        _buffer = std::make_shared<vector<T>>(vec);
+      }
+      _buffer->clear();
+    }
+    _size = 0;
+  }
+
+  // O(1) nothrow
+  void swap(socow_vector& other) noexcept {
+    std::swap(_size, other._size);
+    std::swap(_buffer, other._buffer);
+    std::swap(_data, other._data);
+  }
+
+  // Element access
+
+  // O(1) / O(size); nothrow / strong
+  reference operator[](std::size_t index) {
+    assert(index < size());
+    if (is_small_object()) {
+      return _data[index];
+    } else {
+      if (_buffer.use_count() > 1) {
+        vector<T> vec = *_buffer;
+        _buffer = std::make_shared<vector<T>>(vec);
+      }
+      return (*_buffer)[index];
+    }
+  }
+
+  // O(1) / O(1); nothrow / nothrow
+  const_reference operator[](std::size_t index) const noexcept {
+    assert(index < size());
+    if (is_small_object()) {
+      return _data[index];
+    } else {
+      return (*_buffer)[index];
+    }
+  }
+
+  // O(1) / O(size); nothrow / strong
+  reference front() {
+    return operator[](0);
+  }
+
+  // O(1) / O(1); nothrow / nothrow
+  const_reference front() const noexcept {
+    return operator[](0);
+  }
+
+  // O(1) / O(size); nothrow / strong
+  reference back() {
+    return operator[](size() - 1);
+  }
+
+  // O(1) / O(1); nothrow / nothrow
+  const_reference back() const noexcept {
+    return operator[](size() - 1);
+  }
+
+  // Iterator
+
+  // O(1) / O(size); nothrow / strong
+  iterator begin() {
+    if (is_small_object()) {
+      return _data.begin();
+    } else {
+      if (_buffer.use_count() > 1) {
+        vector<T> vec = *_buffer;
+        _buffer = std::make_shared<vector<T>>(vec);
+      }
+      return _buffer->begin();
+    }
+  }
+
+  // O(1) / O(1); nothrow / nothrow
+  const_iterator begin() const noexcept {
+    if (is_small_object()) {
+      return _data.begin();
+    } else {
+      return _buffer->begin();
+    }
+  }
+
+  // O(1) / O(size); nothrow / strong
+  iterator end() {
+    if (is_small_object()) {
+      iterator begin = _data.begin();
+      std::advance(begin, _size);
+      return begin;
+    } else {
+      if (_buffer.use_count() > 1) {
+        vector<T> vec = *_buffer;
+        _buffer = std::make_shared<vector<T>>(vec);
+      }
+      return _buffer->end();
+    }
+  }
+
+  // O(1) / O(1); nothrow / nothrow
+  const_iterator end() const noexcept {
+    if (is_small_object()) {
+      const_iterator begin = _data.begin();
+      std::advance(begin, _size);
+      return begin;
+    } else {
+      return _buffer->end();
+    }
+  }
+
+public:
+  std::size_t _size;
+
+  union {
+    std::shared_ptr<vector<T>> _buffer;
+    std::array<T, SMALL_SIZE> _data;
+//    T _data[SMALL_SIZE];
+  };
+
+  bool is_small_object() const {
+    return _size <= SMALL_SIZE;
+  }
+  bool is_full() const {
+    return _size == SMALL_SIZE;
+  }
+};
---
+++ src/vector.h
@@ -0,0 +1,291 @@
+#pragma once
+
+#include <algorithm>
+#include <cstddef>
+#include <new>
+
+template <typename T>
+class vector {
+public:
+  using value_type = T;
+
+  using reference = T&;
+  using const_reference = const T&;
+
+  using pointer = T*;
+  using const_pointer = const T*;
+
+  using iterator = pointer;
+  using const_iterator = const_pointer;
+
+public:
+  // O(1) nothrow
+  vector() noexcept
+      : _capacity(0)
+      , _size(0)
+      , _data(nullptr) {}
+
+  // O(N) strong
+  vector(const vector& other)
+      : _capacity(other.size())
+      , _size(other.size())
+      , _data(nullptr) {
+    if (_capacity > 0) {
+      auto tmp = create_tmp(other, other.capacity());
+      _data = tmp;
+    }
+  }
+
+  // O(1) strong
+  vector(vector&& other) noexcept
+      : _capacity(other.size())
+      , _size(other.size())
+      , _data(other.data()) {
+    other._capacity = 0;
+    other._size = 0;
+    other._data = nullptr;
+  }
+
+  // O(N) strong
+  template <typename Array>
+  vector (const Array& other, std::size_t new_capacity)
+      : _capacity(other.size())
+      , _size(other.size())
+      , _data(nullptr) {
+    if (_capacity > 0) {
+      auto tmp = create_tmp(other, new_capacity);
+      _data = tmp;
+    }
+  }
+
+  // O(N) strong
+  vector& operator=(const vector& other) {
+    if (this == &other) {
+      return *this;
+    }
+    vector(other).swap(*this);
+    return *this;
+  }
+
+  // O(1) strong
+  vector& operator=(vector&& other) noexcept {
+    if (this == &other) {
+      return *this;
+    }
+    swap(other);
+    return *this;
+  }
+
+  // O(N) nothrow
+  ~vector() noexcept {
+    data_clear(data(), size());
+  }
+
+  // O(1) nothrow
+  reference operator[](size_t index) {
+    return *(_data + index);
+  }
+
+  // O(1) nothrow
+  const_reference operator[](size_t index) const {
+    return *(_data + index);
+  }
+
+  // O(1) nothrow
+  pointer data() noexcept {
+    return _data;
+  }
+
+  // O(1) nothrow
+  const_pointer data() const noexcept {
+    return _data;
+  }
+
+  // O(1) nothrow
+  size_t size() const noexcept {
+    return _size;
+  }
+
+  // O(1) nothrow
+  reference front() {
+    return *(data());
+  }
+
+  // O(1) nothrow
+  const_reference front() const {
+    return *(data());
+  }
+
+  // O(1) nothrow
+  reference back() {
+    return *(data() + size() - 1);
+  }
+
+  // O(1) nothrow
+  const_reference back() const {
+    return *(data() + size() - 1);
+  }
+
+  // O(1)* strong
+  void push_back(const T& value) {
+    if (_size == _capacity) {
+      auto tmp = create_tmp(*this, _capacity * 2 + 1);
+      try {
+        new (tmp + size()) value_type(value);
+      } catch (...) {
+        data_clear(tmp, size());
+        throw;
+      }
+      data_clear(data(), size());
+      _data = tmp;
+      _capacity = _capacity * 2 + 1;
+      ++_size;
+    } else {
+      new (data() + size()) value_type(value);
+      ++_size;
+    }
+  }
+
+  // O(1) nothrow
+  void pop_back() noexcept {
+    data()[size() - 1].~value_type();
+    --_size;
+  }
+
+  // O(1) nothrow
+  bool empty() const noexcept {
+    return size() == 0;
+  }
+
+  // O(1) nothrow
+  size_t capacity() const noexcept {
+    return _capacity;
+  }
+
+  // O(N) strong
+  void reserve(size_t new_capacity) {
+    if (new_capacity > capacity()) {
+      change_capacity(new_capacity);
+    }
+  }
+
+  // O(N) strong
+  void shrink_to_fit() {
+    if (size() < capacity()) {
+      change_capacity(size());
+    }
+  }
+
+  // O(N) nothrow
+  void clear() noexcept {
+    for (size_t j = _size; j > 0; --j) {
+      _data[j - 1].~T();
+    }
+    _size = 0;
+  }
+
+  // O(1) nothrow
+  void swap(vector& other) noexcept {
+    std::swap(_capacity, other._capacity);
+    std::swap(_size, other._size);
+    std::swap(_data, other._data);
+  }
+
+  // O(1) nothrow
+  iterator begin() noexcept {
+    return data();
+  }
+
+  // O(1) nothrow
+  iterator end() noexcept {
+    return data() + size();
+  }
+
+  // O(1) nothrow
+  const_iterator begin() const noexcept {
+    return data();
+  }
+
+  // O(1) nothrow
+  const_iterator end() const noexcept {
+    return data() + size();
+  }
+
+  // O(N) strong
+  iterator insert(const_iterator pos, const T& value) {
+    size_t idx = pos - begin();
+
+    push_back(value);
+
+    iterator it = end() - 1;
+    while (it != begin() + idx) {
+      std::iter_swap(it - 1, it);
+      --it;
+    }
+    return begin() + idx;
+  }
+
+  // O(N) nothrow(swap)
+  iterator erase(const_iterator pos) noexcept {
+    return erase(pos, pos + 1);
+  }
+
+  // O(N) nothrow(swap)
+  iterator erase(const_iterator first, const_iterator last) noexcept {
+    if (first == last) {
+      return iterator(last);
+    }
+    size_t idx = first - begin();
+    iterator left = iterator(first);
+    iterator right = iterator(last);
+
+    while (right != end()) {
+      std::iter_swap(left, right);
+      ++right;
+      ++left;
+    }
+    for (; left != end(); ++left) {
+      left->~value_type();
+    }
+
+    _size -= last - first;
+    return begin() + idx;
+  }
+
+private:
+  size_t _capacity;
+  size_t _size;
+  pointer _data;
+
+  // O(N) nothrow
+  static void data_clear(pointer memory, size_t idx) noexcept {
+    for (size_t j = idx; j > 0; --j) {
+      memory[j - 1].~value_type();
+    }
+    operator delete(memory);
+  }
+
+  // O(N) strong
+  template <typename Array>
+  static pointer create_tmp(const Array& source, size_t new_capacity) {
+    auto tmp = static_cast<pointer>(operator new(sizeof(value_type) * new_capacity));
+    size_t i = 0;
+    try {
+      for (vector::const_iterator it = source.begin(); i < source.size() && i < new_capacity; ++i, ++it) {
+        new (tmp + i) value_type(*it);
+      }
+    } catch (...) {
+      data_clear(tmp, i);
+      throw;
+    }
+    return tmp;
+  }
+
+  // O(N) strong
+  void change_capacity(size_t new_capacity) {
+    auto tmp = create_tmp(*this, new_capacity);
+    data_clear(data(), size());
+    _data = tmp;
+    _capacity = new_capacity;
+  }
+};
